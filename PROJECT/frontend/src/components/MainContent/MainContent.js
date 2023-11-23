6f9e418b245ccbc8abfe4c38de28ba561df19838
Benedict Tan <bendamian2012@gmail.com>
2023-11-22 23:02:00 -0800

    Added conditional rendering for MainContent

--- PROJECT/frontend/src/components/MainContent/MainContent.js
+++ PROJECT/frontend/src/components/MainContent/MainContent.js
@@ -1,12 +1,17 @@
 import './MainContent.css'
 import sidebarLinks from '../../links.js'
+import Shows from '../Shows/Shows.js'
 
 function MainContent({selectedId}) {
-  return (
-    <div className="maincontent">
-      {selectedId}
-    </div>
-  );
+
+  const title = sidebarLinks[selectedId].title;
+
+  if (title == "SHOWS"){
+    return <Shows /> 
+  } else {
+    return <div className="maincontent">{selectedId}</div>
+  }
+
 }
 
 export default MainContent;
---
+++ PROJECT/frontend/src/components/Shows/Shows.js
@@ -0,0 +1,14 @@
+import './Shows.css'
+import React, { useState, useEffect } from 'react';
+
+function Shows() {
+  const [shows, setShows] = useState(null);
+
+  return (
+    <div className="shows maincontent">
+      shows
+    </div>
+  );
+}
+
+export default Shows;
