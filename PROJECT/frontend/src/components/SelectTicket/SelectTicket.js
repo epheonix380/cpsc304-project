fca1243455e13c18325e809d0333d1323530a73d
mishareyes <hellomboreyes@gmail.com>
2023-11-29 16:15:44 -0800

    refactor modal dropdown to list available tickets

--- PROJECT/frontend/src/components/SelectTicket/SelectTicket.js
+++ PROJECT/frontend/src/components/SelectTicket/SelectTicket.js
@@ -1,26 +1,57 @@
 import './SelectTicket.css'
 import React, {useEffect, useState} from "react";
-import Filters from '../Filters/Filters.js'
-import {Table, Modal, InputNumber, Checkbox} from "antd";
+import {Modal, Select} from "antd";
 
 function SelectTicket({selectedTicket}) {
     const [isSelectModalOpen, setIsSelectModalOpen] = useState(false);
     const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+    const [oldTicket, setOldTicket] = useState({});
+    const [newTicket, setNewTicket] = useState({});
+    const [ticketToDelete, setTicketToDelete] = useState({});
+    const ticketNumber = selectedTicket.length === 0 ? undefined : selectedTicket[0].ticketid;
+    const eventname = selectedTicket.length === 0 ? '' : selectedTicket[0].eventname;
+    let newTicketNumber = 0;
+
+    // need a useEffect() to set availableTickets that will show in dropdown
+    // pass in eventid & userid, make sure tickets are not yet Issued
+
+    useEffect(() => {
+        console.log(oldTicket);
+        console.log(newTicket);
+    }, [oldTicket, newTicket]) // when newTicket is updated, that means send update query
+
+    useEffect(() => {
+        console.log(ticketToDelete);
+    }, [ticketToDelete]) // when ticketToDelete is updated, that means send delete query
+
+    const availableTickets = [
+        { value: 0, label: 'seatnumber: 1, rownumber: 2, sectionnumber: 3' },
+        { value: 1, label: 'seatnumber: 4, rownumber: 5, sectionnumber: 6' },
+        { value: 2, label: 'seatnumber: 7, rownumber: 8, sectionnumber: 9' },
+        { value: 4, label: 'seatnumber: 10, rownumber: 20, sectionnumber: 30' },
+    ]
 
     const showModal = (type) => {
-        if (type === 'select') {
+        if (ticketNumber === undefined) {
+            alert("Please select a ticket first!");
+        } else if (type === 'select') {
             setIsSelectModalOpen(true);
         } else {
             setIsDeleteModalOpen(true);
         }
     };
+
     const handleOk = (type) => {
         if (type === 'select') {
+            setOldTicket({ticketid: ticketNumber});
+            setNewTicket({ticketid: newTicketNumber});
             setIsSelectModalOpen(false);
         } else {
+            setTicketToDelete({ticketid: ticketNumber});
             setIsDeleteModalOpen(false);
         }
     };
+
     const handleCancel = (type) => {
         if (type === 'select') {
             setIsSelectModalOpen(false);
@@ -28,25 +59,19 @@ function SelectTicket({selectedTicket}) {
             setIsDeleteModalOpen(false);
         }
     };
-    const onChange = (value) => {
-        console.log('checked = ', value);
-    };
-    const ticketNumber = selectedTicket.length === 0 ? 0 : selectedTicket[0].ticketid;
-    const eventname = selectedTicket.length === 0 ? '' : selectedTicket[0].eventname;
-    // TODO: add error handling for if a ticket is not selected
 
     return (
         <div>
-            <Modal title={`Updating ticket #${ticketNumber}`}
+            <Modal title={`Updating ticket #${ticketNumber}`} destroyOnClose={true}
                    open={isSelectModalOpen} onOk={() => handleOk('select')} onCancel={() => handleCancel('select')}>
-                <p>What seat number would you like?</p>
-                <InputNumber min={1} max={10} defaultValue={3} onChange={onChange}/>
-                <p>What row number would you like?</p>
-                <InputNumber min={1} max={10} defaultValue={3} onChange={onChange}/>
-                <p>What section number would you like?</p>
-                <InputNumber min={1} max={10} defaultValue={3} onChange={onChange}/>
+                <p>What seat would you like to change to?</p>
+                <Select
+                    options={availableTickets}
+                    defaultValue={0}
+                    onChange={(e) => newTicketNumber = e}
+                />
             </Modal>
-            <Modal title={`Are you sure you want to sell ticket #${ticketNumber}?`}
+            <Modal title={`Are you sure you want to delete ticket #${ticketNumber}?`}
                    open={isDeleteModalOpen} onOk={() => handleOk('delete')} onCancel={() => handleCancel('delete')}>
                 <p>This is for the event: {eventname}</p>
             </Modal>
